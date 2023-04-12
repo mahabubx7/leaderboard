2ef9b6f45ebb61afc9a14ad9fbb06118169feb0b
Mahabub Alam <mahabubx7@gmail.com>
2023-04-12 16:18:52 +0600

    :hammer: added:env-vars-for-api

---
+++ src/config.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export const API = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+export const API_KEY = 'Zl4d7IVkemOTTVg2fUdz';
